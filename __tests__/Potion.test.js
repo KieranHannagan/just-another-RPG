6901398441a8878b8be1c25a3a4b8ec27b915720
KieranHannagan <kieranhan1999@gmail.com>
2021-12-26 18:07:57 -0600

    Wrote potion constructer

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,20 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+function Potion(name) {
+    this.name = name;
+  
+    if (this.name === 'health') {
+      this.value = Math.floor(Math.random() * 10 + 30);
+    } else {
+      this.value = Math.floor(Math.random() * 5 + 7);
+    }
+  }
+  
+  module.exports = Potion;
